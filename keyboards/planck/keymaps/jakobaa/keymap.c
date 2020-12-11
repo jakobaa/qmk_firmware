05196aed1acf2de125991a2a86eba222484174d0
Jakob Aasa <jakob@aasa.xyz>
2020-12-11 21:21:56 +0100

    move colon/semicolon to home row

--- keyboards/planck/keymaps/jakobaa/keymap.c
+++ keyboards/planck/keymaps/jakobaa/keymap.c
@@ -113,10 +113,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
-    KC_DEL,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
-    _______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,  KC_LPRN, KC_RPRN, KC_BSPC,
+    KC_DEL,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,  KC_LCBR, KC_RCBR, KC_PIPE,
+    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_SCLN,    KC_COLN,   KC_HOME, KC_END,  _______,
+    _______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,  KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
 /* Raise
